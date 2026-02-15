90c7a6edfd02617104223a3ff081661205a7560d
erosMariano <erosmariano1@gmail.com>
2026-02-15 09:52:09 -0300

    feat (Nosso Diferencial): Criação de estrutura por que somos diferentes

--- assets/script.js
+++ assets/script.js
@@ -333,4 +333,40 @@ window.addEventListener("load", () => {
     yoyo: true,
     ease: "power1.inOut",
   });
+
+  // Sobre nós
+
+  let cards = gsap.utils.toArray(".stackCard");
+
+  let stickDistance = 0;
+
+  let firstCardST = ScrollTrigger.create({
+    trigger: cards[0],
+    start: "center center",
+  });
+
+  let lastCardST = ScrollTrigger.create({
+    trigger: cards[cards.length - 1],
+    start: "center center",
+  });
+
+  cards.forEach((card, index) => {
+    var scale = 1 - (cards.length - index) * 0.025;
+    let scaleDown = gsap.to(card, {
+      scale: scale,
+      "transform-origin": '"50% ' + (lastCardST.start + stickDistance) + '"',
+    });
+
+    ScrollTrigger.create({
+      trigger: card,
+      start: "center center",
+      end: () => lastCardST.start + stickDistance,
+      pin: true,
+      markers: false,
+      pinSpacing: false,
+      ease: "none",
+      animation: scaleDown,
+      toggleActions: "restart none none reverse",
+    });
+  });
 });
